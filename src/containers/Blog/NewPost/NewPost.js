a8d01bbf9bc753d7c5800ba7ded0938c65a05cdd
Ontorok <nasir.amath07@gmail.com>
2020-12-07 12:12:17 +0600

    Title color change in new post

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -29,7 +29,7 @@ class NewPost extends Component {
     render() {
         return (
             <div className="NewPost">
-                <h1>Add a Post</h1>
+                <h1 style={{ color: "skyblue" }}>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
                 <label>Content</label>
